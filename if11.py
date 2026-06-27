a626e3b287976bc221a2dde0bdc077e764062bf4
Asrorbek0516 <asrorbekalimqulov@mail.com>
2026-06-27 17:51:24 +0500

    if 2-misoli

---
+++ if11.py
@@ -0,0 +1,12 @@
+a = int(input("a= "))
+b = int(input("b= "))
+
+if a !=b:
+    if a>b:
+        print("a= ",a)
+        print("b= ",a)
+    else:
+       print("a= ",b)
+       print("b= ",b) 
+else:
+    print(0)
