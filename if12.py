3b2d28c073dcd71d028f244df502bbc958002489
Asrorbek0516 <asrorbekalimqulov@mail.com>
2026-06-30 09:40:11 +0500

    if12 shunisi togri

--- if12.py
+++ if12.py
@@ -2,9 +2,9 @@ a = int(input("a= "))
 b = int(input("b= "))
 c = int(input("c= "))
 
-if a>b and a>c:
-    print(f"kattasi= {a}")
-elif b>a and b>c:
-    print(f"kattasi= {b}")
+if a<b and a<c:
+    print(f"kichigi= {a}")
+elif b<a and b<c:
+    print(f"kichigi= {b}")
 else:
-    print(f"kattasi= {c}")
+    print(f"kichigi= {c}")
